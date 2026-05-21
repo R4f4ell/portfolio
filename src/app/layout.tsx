0d04f63f39c9ba5b409914c5d662d438ac30f58e
Rafael <132296553+R4f4ell@users.noreply.github.com>
2026-05-21 18:06:23 -0300

    Comecando novamente o portfolio

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "Portfolio pessoal",
+  icons: {
+    icon: "/iconFavicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
