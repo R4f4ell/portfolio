01a27b228702d65810d5919093e714aafa40cafb
Rafael <132296553+R4f4ell@users.noreply.github.com>
2026-05-22 09:48:40 -0300

    chore: estrutura inicial do portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
+import { HeroSection } from "@/components/sections/inicio/Inicio";
+import { AboutSection } from "@/components/sections/sobre/Sobre";
+import { SkillsSection } from "@/components/sections/habilidades/Habilidades";
+import { ProjectsSection } from "@/components/sections/projetos/Projetos";
+import { ContactSection } from "@/components/sections/contato/Contato";
+
 export default function Home() {
-  return null;
+  return (
+    <>
+      <Header />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </>
+  );
 }
